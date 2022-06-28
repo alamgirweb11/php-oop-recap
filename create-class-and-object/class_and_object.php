633e0fe8acec461b251c2705c0c6034e095f16ad
alamgirweb11 <alamgirweb11@gmail.com>
2022-06-28 23:23:11 +0600

    make class and object

---
+++ create-class-and-object/class_and_object.php
@@ -0,0 +1,8 @@
+<?php
+// create class
+class TestClass{
+       public $greetings  = "Have a good day!";
+}
+// create object
+$test_class = new TestClass();
+echo $test_class->greetings; // output: Have a good day!
