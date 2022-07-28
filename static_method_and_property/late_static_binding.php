e89e41fa0049afe61bea8c9eb511923ebd7408a4
alamgirweb11 <alamgirweb11@gmail.com>
2022-07-29 00:03:37 +0600

    solution of self keyword using late binding method

--- static_method_and_property/late_static_binding.php
+++ static_method_and_property/late_static_binding.php
@@ -1,12 +1,30 @@
 <?php  
 
 // problem code 
+// class StudentInfo 
+// {
+//      protected static $mobileNumber = "0120000000";
+
+//      public static function getMobileNumber(){
+//          echo self::$mobileNumber;
+//      }
+// }
+
+// class StudentInfoUpdate extends StudentInfo 
+// {
+//     protected static $mobileNumber = "012045510";
+// }
+
+// StudentInfoUpdate::getMobileNumber(); // output:  0120000000 not 012045510
+
+
+// solution code
 class StudentInfo 
 {
      protected static $mobileNumber = "0120000000";
 
      public static function getMobileNumber(){
-         echo self::$mobileNumber;
+         echo static::$mobileNumber;
      }
 }
 
@@ -15,4 +33,4 @@ class StudentInfoUpdate extends StudentInfo
     protected static $mobileNumber = "012045510";
 }
 
-StudentInfo::getMobileNumber(); // output:  0120000000 not 012045510
+StudentInfoUpdate::getMobileNumber();  // output: 012045510 not 0120000000
